0ae16f509b45bf7332dd77435b5824e0cbb4ee64
Krzysztof Mikuta <krzysztof.mikuta16@gmail.com>
2025-02-09 15:25:20 +0100

    showing genders in nouns

--- mllab/llm/francais/data/prompts.py
+++ mllab/llm/francais/data/prompts.py
@@ -3,12 +3,13 @@ system_prompt = "You are an assistant helping to learn french used in Switzerlan
 word_prompt_parts = [
     "Indicate if it is verb, noun, adjective or number",
     "Also provide phonetic transcription.",
-    "When it is number, provide also the ordinal number."
+    "When it is number, provide also the ordinal number.",
+    "When it is noun, provide gender."
     "Use the format: word [transcription], verb | noun | adjective | number."
 ]
 
 word_prompt = lambda w: f"Specify how to say '{w}' in french. " + " ".join(word_prompt_parts)
 
-conjugation_prompt = lambda w, t: f"Provide conjugation for '{w}' by pronouns in {t}. Use list format. Do not provide transcription."
+conjugation_prompt = lambda w, t: f"Provide conjugation for '{w}' by pronouns in {t}. Use list format."
 
 __all__ = ["system_prompt", "conjugation_prompt", "word_prompt"]
